49784c45ae1a699ac35ebf47788e59f3a8874663
pavinane <pavimegan77@gmail.com>
2022-08-30 19:18:12 +0530

    add api but getting error

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,8 +6,8 @@ import { Search } from "@mui/icons-material";
 const SearchBar = () => {
   return (
     <Paper
-      component="from"
-      onSubmit={{}}
+      component="form"
+      onSubmit={() => {}}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -18,17 +18,14 @@ const SearchBar = () => {
     >
       <input
         className="search-bar"
-        placeholder="Search ..."
+        placeholder="Search..."
         value=""
-        onChange={{}}
+        onChange={() => {}}
       />
       <IconButton
         type="submit"
-        i
-        sx={{
-          p: "10px",
-          color: "red",
-        }}
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
       >
         <Search />
       </IconButton>
